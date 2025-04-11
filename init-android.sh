3d2cff1aa58dea43613b7a947d8b3ee5294c37ad
Li Dongmin <ldm200026@live.cn>
2025-04-12 01:47:08 +0800

    Update init-android.sh

--- init-android.sh
+++ init-android.sh
@@ -17,9 +17,9 @@
 #
 
 # IJK_FFMPEG_UPSTREAM=git://git.videolan.org/ffmpeg.git
-IJK_FFMPEG_UPSTREAM=https://gitee.com/mahongyin/ff6.1.1.git
-IJK_FFMPEG_FORK=https://gitee.com/mahongyin/ff6.1.1.git
-IJK_FFMPEG_COMMIT=ff6.1.1--ijk0.8.8--20241121--001
+IJK_FFMPEG_UPSTREAM=https://github.com/mytv-android/FFmpeg.git
+IJK_FFMPEG_FORK=https://github.com/mytv-android/FFmpeg.git
+IJK_FFMPEG_COMMIT=master
 IJK_FFMPEG_LOCAL_REPO=extra/ffmpeg
 
 set -e
